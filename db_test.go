c3f293358e201d85f717756995d59f1144a96ba7
Roan Brand <roan.brand@imqs.co.za>
2018-05-07 14:54:53 +0200

    add db test

---
+++ db_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+	"runtime"
+)
+
+const dsn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=A:/Projects/SpectroDisplay/AccessDB Example/SpvDB_MeasureResults.mdb;"
+const numResults = 100
+const numRetrievals = 1000
+
+func logStats(t *testing.T) {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	t.Logf("\nAlloc = %v\nTotalAlloc = %v\nSys = %v\nNumGC = %v\n\n", m.Alloc / 1024, m.TotalAlloc / 1024, m.Sys / 1024, m.NumGC)
+}
+
+func TestResultsRetrieval(t *testing.T) {
+	logStats(t)
+
+	err := startDBConn(dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < numRetrievals; i++ {
+		_, err := queryResults(numResults)
+		if err != nil {
+			logStats(t)
+			t.Fatalf("Error retrieving results on iteration %d: %s", i, err)
+		}
+	}
+	logStats(t)
+}
